0d0f26b77cdc9d32c3ac61cd2e302ad79c49ff4f
ksfallon <fallon.katelin@gmail.com>
2021-05-06 11:04:05 -0400

    added pseudo coding to help get started

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// Search for the cities on the lefthand side
+// fetch localstorage city names, and display to website.
+// box for user to put in the city HTML
+// button for them to click to search HTML
+// click addEventListener for the button - 
+    // save value in box to a variable
+// fetch API for the GEOLOCATION w/n open weather which gives you the Long and Lat coordinates
+// use coordinate results for next API which is the openweather One Call.
+// MAKE LINES 7 & 8 a function so it can be called later on.
+// Do something with results - local storage, screen display.
