8eb3ad39799b7e57f178e89af6c4f685be35c231
ksfallon <fallon.katelin@gmail.com>
2021-05-06 14:46:44 -0400

    updated geoCode API, currently pulls data into local storage, need to create HTML for right side of web page so i can pull information to display

--- js/script.js
+++ js/script.js
@@ -2,43 +2,49 @@
 // fetch localstorage city names, and display to website.
 // box for user to put in the city HTML
 // button for them to click to search HTML
-var searchFormE1 = document.querySelector("#search-form");
-var searchButton = document.querySelector("#search-button");
-var cityInput = document.querySelector('#search-input')
-var searchCityList = document.querySelector(".search-history");
+var searchFormE1 = document.querySelector('#search-form');
+var searchButton = document.querySelector('#search-button');
+var cityInput = document.querySelector('#search-input');
+var searchCityList = document.querySelector('.search-history');
 
 function handleCitySearchSubmit(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    var searchInputVal = cityInput.value;
-    console.log("city;", searchInputVal);
+  var searchInputVal = cityInput.value;
 
-    localstorage.setItem("city", searchInputVal);
+  console.log('city;', searchInputVal);
+
+  localStorage.setItem('city', searchInputVal);
+
+  // don't I need to append as buttons though?
+// also this is only going to give what is searched -
+// NEED TO APPEND FROM LOCAL STORAGE probably using a for loop
+// and put this somewhere else, right when page opens
+  var node = document.createElement('li');
+  var textnode = document.createTextNode(searchInputVal);
+  node.appendChild(textnode);
+  document.querySelector('.search-history').appendChild(node);
+
+
+  var geocodeQueryUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${searchInputVal},&limit=5&appid=af8f9e641174c07751bae2f5bbbc3fb5`;
+  $.ajax({ url: geocodeQueryUrl })
+  
+  .then(function (response) {
+    console.log('response:', response);
+    var cityToSearch = response[0];
+    var oneCallWeatherApi = `https://api.openweathermap.org/data/2.5/onecall?lat=${cityToSearch.lat}&lon=${cityToSearch.lon}&appid=af8f9e641174c07751bae2f5bbbc3fb5`;
+    $.ajax({ url: oneCallWeatherApi })
     
-    // don't I need to append as buttons though?
-    var node = document.createElement("li");
-    var textnode = document.createTextNode(searchInputVal);
-    node.appendChild(textnode);
-    document.getElementsByClassName("search-history").appendChild(node);
-
+    .then(function (response) {
+      console.log('WEATHER DATA!:', response);
+    });
+  });
 }
-// click addEventListener for the button - 
-    // save value in box to a variable
-searchButton.addEventListener("click", handleCitySearchSubmit);
-
-
-// function locationApiSearch(city, state) {
-//     var geocodeQueryUrl = "http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},&limit=5&appid=af8f9e641174c07751bae2f5bbbc3fb5";
-
-//     if (city) {
-//         geocodeQueryUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + city +
-//     }
-
-//     geocodeQueryUrl = geocodeQueryUrl + state +
-// } 
-// click addEventListener for the button - 
-    // save value in box to a variable
-
+// click addEventListener for the button -
+// save value in box to a variable
+searchButton.addEventListener('click', handleCitySearchSubmit);
+// click addEventListener for the button -
+// save value in box to a variable
 // fetch API for the GEOLOCATION w/n open weather which gives you the Long and Lat coordinates
 // use coordinate results for next API which is the openweather One Call.
 // MAKE LINES 8 & 9 a function so it can be called later on.
